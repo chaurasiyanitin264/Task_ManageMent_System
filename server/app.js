b0250bac2a1d55d55a40cc00785d3ad0f35aef62
Nitin Chaurasiya <chaurasiyanitin264@gmail.com>
2025-01-24 23:26:04 +0530

    TaskManagemt

--- server/app.js
+++ server/app.js
@@ -2,15 +2,18 @@ const express= require("express");
 const app= express();
 const cors= require("cors");
 const mongoose=require("mongoose");
+require("dotenv").config();
 const adminRoute= require("./routes/adminRoutes");
 const bodyparser = require('body-parser');
+const port =process.env.PORT ||8000;
+const MNDB=process.env.MGCONN;
 
 app.use(cors());
 app.use(bodyparser.urlencoded({ extended: true }))
 app.use(bodyparser.json())
 
 
-mongoose.connect("mongodb://127.0.0.1:27017/TaskManagement").then(()=>{
+mongoose.connect(MNDB).then(()=>{
     console.log("DB connected!");
 })
 
